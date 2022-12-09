d60a31ec9653fa59515a642637fecb66d58ee4cc
Mihai-Razvan Jeler <mjeler@talentingsoftware.com>
2022-12-09 14:22:54 +0200

    Home page

--- WorkoutScheduler/WorkoutScheduler/ClientApp/src/components/Home.js
+++ WorkoutScheduler/WorkoutScheduler/ClientApp/src/components/Home.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import video from '../assets/videos/home-video.mp4'
+import './home.css'
 
 export class Home extends Component {
   static displayName = Home.name;
 
   render() {
     return (
-      <div>
+      <div className='home'>
+        <video src={video} autoPlay loop muted/>
+        <div className='content'>
+            <p>Welcome to the </p>
+            <h1>Workout Scheduler</h1>
+        </div>
       </div>
     );
   }
